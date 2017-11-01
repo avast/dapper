70dcdf207102b062eb365cbe01cfb1918c1b20f6
kolena <kolena@avast.com>
2017-11-01 09:50:33 +0100

    moved queryoptions to own file

--- core/src/main/scala/com/avast/dapper/CassandraDao.scala
+++ core/src/main/scala/com/avast/dapper/CassandraDao.scala
@@ -1,9 +1,5 @@
 package com.avast.dapper
 
-import java.time.{Duration, Instant}
-
-import com.datastax.driver.core.ConsistencyLevel
-
 import scala.concurrent.Future
 
 trait CassandraDao[PrimaryKey, Entity <: CassandraEntity[PrimaryKey]] {
@@ -16,25 +12,4 @@ trait CassandraDao[PrimaryKey, Entity <: CassandraEntity[PrimaryKey]] {
   def save(instance: Entity, queryOptions: WriteOptions = WriteOptions.Default): Future[Unit]
 
   def delete(instance: Entity, queryOptions: DeleteOptions = DeleteOptions.Default): Future[Unit]
-}
-
-case class ReadOptions(consistencyLevel: Option[ConsistencyLevel] = None)
-
-case class WriteOptions(ttl: Option[Duration] = None,
-                        timestamp: Option[Instant] = None,
-                        consistencyLevel: Option[ConsistencyLevel] = None,
-                        ifNotExist: Boolean = false)
-
-case class DeleteOptions(timestamp: Option[Instant] = None, consistencyLevel: Option[ConsistencyLevel] = None, ifExists: Boolean = false)
-
-object ReadOptions {
-  val Default: ReadOptions = ReadOptions()
-}
-
-object WriteOptions {
-  val Default: WriteOptions = WriteOptions()
-}
-
-object DeleteOptions {
-  val Default: DeleteOptions = DeleteOptions()
 }
